db9c7915bf8c58b459aa3b6da17ba1ecb626df59
Yannick Schurter <yannick.schurter@gmail.com>
2018-07-12 13:34:04 +0200

    first version of robot detector

---
+++ robots/index.js
@@ -0,0 +1,104 @@
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+
+/**
+ * Detect robots by counting accesses
+ */
+module.exports = function () {
+  const self    = this;
+  const report  = this.report;
+  const req     = this.request;
+  const jobPath = this.job.jobPath;
+
+  const cache = ezpaarse.lib('cache')(`robots-${this.job.jobID}`);
+  const ttl = parseInt(req.header('robots-ttl')) || 3600 * 24;
+  const threshold = parseInt(req.header('robots-threshold')) || 100;
+
+  if (!cache) {
+    const err = new Error('failed to connect to mongodb, cache not available for robots');
+    err.status = 500;
+    return err;
+  }
+
+  return new Promise(function (resolve, reject) {
+    cache.checkIndexes(ttl, function (err) {
+      if (err) {
+        self.logger.error(`robots: failed to ensure indexes : ${err}`);
+        return reject(new Error('failed to ensure indexes for the cache of robots'));
+      }
+
+      resolve(process);
+    });
+  });
+
+  /**
+   * Count accesses
+   * @param  {object} ec the EC to process, null if no EC left
+   * @param  {Function} next the function to call when we are done with the given EC
+   */
+  function process(ec, next) {
+    if (!ec) {
+      return persist(() => {
+        clearCache(() => next(null));
+      });
+    }
+
+    const trackCode = ec['hal_anonymizer_track_code'];
+    if (!trackCode) { return next(); }
+
+    increment(trackCode, true, next);
+  }
+
+  /**
+   * Increment counter for a given trackcode
+   * @param {String} trackCode
+   * @param {Boolean} retry  retry in case of error, workaround for concurrent upserts
+   * @param {Function} callback
+   */
+  function increment(trackCode, retry, callback) {
+    cache.collection.update({ id: trackCode }, { $inc: { counter: 1 } }, { upsert: true }, err => {
+      if (err && retry) {
+        return increment(trackCode, false, callback);
+      }
+      if (err) {
+        self.logger.error(`robots: failed to increment count for track code ${trackCode} : ${err}`);
+      }
+      callback();
+    });
+  }
+
+  /**
+   * Clear the collection
+   * @param {Function} callback
+   */
+  function clearCache(callback) {
+    cache.collection.drop(err => {
+      if (err) { self.logger.error('robots: failed to clear cache'); }
+      callback();
+    });
+  }
+
+  /**
+   * Get track codes with high number of accesses and persist them into a file
+   * @param {Function} callback
+   */
+  function persist(callback) {
+    cache.collection.find({ counter: { $gt: threshold } }).toArray((err, docs) => {
+      if (err) {
+        self.logger.error('robots: failed to persist robots');
+        return callback();
+      }
+
+      report.set('general', 'robots-number', docs.length);
+
+      fs.writeFile(path.resolve(jobPath, 'robots.json'), JSON.stringify(docs, null, 2), err => {
+        if (err) {
+          self.logger.error('robots: failed to persist robots');
+        }
+        return callback();
+      });
+    });
+  }
+};
