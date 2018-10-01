71cf7050d0c2819e6232aa472b6dd1ef9fb31917
Yannick Schurter <yannick.schurter@gmail.com>
2018-10-01 18:04:57 +0200

    add cache mocking

--- mock.js
+++ mock.js
@@ -27,3 +27,39 @@ exports.contextify = function (middleware) {
 
   return middleware.call(ctx);
 };
+
+function cache(collectionName) {
+  const items = new Map();
+
+  const cacheObject = {
+    collection: null
+  };
+
+  cacheObject.checkIndexes = function (ttl, callback) {
+    callback();
+  };
+
+  cacheObject.set = function (id, doc, callback) {
+    items.set(id, {
+      date: new Date(),
+      id: id,
+      data: doc
+    });
+    callback();
+  };
+
+  cacheObject.get = function (id, callback) {
+    const item = items.get(id);
+    callback(null, item && item.data);
+  };
+
+  return cacheObject;
+}
+
+global.ezpaarse = {};
+global.ezpaarse.config = {};
+
+const libs = { cache };
+global.ezpaarse.lib = function (name) {
+  return libs[name];
+};
