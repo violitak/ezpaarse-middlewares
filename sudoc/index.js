fd5d810e3ec11cec0d7d51c45b334187c96f585a
Yannick Schurter <yannick.schurter@gmail.com>
2019-01-11 11:41:03 +0100

    lint

--- sudoc/index.js
+++ sudoc/index.js
@@ -1,29 +1,29 @@
 'use strict';
 
-var cache = ezpaarse.lib('cache')('sudoc');
-var sudoc = require('sudoc');
+const cache = ezpaarse.lib('cache')('sudoc');
+const sudoc = require('sudoc');
 
-var issnPattern = /^[0-9]{4}-[0-9xX]{4}$/i;
-var isbnPattern = /^(97(8|9))?\d{9}(\d|X)$/i;
+const issnPattern = /^[0-9]{4}-[0-9xX]{4}$/i;
+const isbnPattern = /^(97(8|9))?\d{9}(\d|X)$/i;
 
 /**
  * Enrich ECs with HAL data
  */
 module.exports = function () {
-  var activated = (this.request.header('sudoc-enrich') || '').toLowerCase() === 'true';
-  var throttle  = parseInt(this.request.header('sudoc-throttle')) || 500;
-  var ttl       = parseInt(this.request.header('sudoc-ttl')) || 3600 * 24 * 7;
+  const activated = (this.request.header('sudoc-enrich') || '').toLowerCase() === 'true';
+  const throttle  = parseInt(this.request.header('sudoc-throttle')) || 500;
+  const ttl       = parseInt(this.request.header('sudoc-ttl')) || 3600 * 24 * 7;
 
   if (!activated) {
     this.logger.verbose('Sudoc enrichment not activated');
     return function (ec, next) { next(); };
   }
 
-  var self     = this;
-  var report   = this.report;
-  var pending  = new Map();
-  var buffer   = [];
-  var busy     = false;
+  const self    = this;
+  const report  = this.report;
+  const pending = new Map();
+  const buffer  = [];
+  let busy      = false;
 
   self.logger.verbose('Sudoc enrichment activated');
   self.logger.verbose('Sudoc throttle: %dms', throttle);
@@ -34,7 +34,7 @@ module.exports = function () {
   report.set('general', 'sudoc-enriched-ecs', 0);
 
   if (!cache) {
-    var err = new Error('failed to connect to mongodb, cache not available for sudoc');
+    const err = new Error('failed to connect to mongodb, cache not available for sudoc');
     err.status = 500;
     return err;
   }
@@ -43,13 +43,13 @@ module.exports = function () {
    * Pull the next EC to enrich with sudoc
    */
   function pullBuffer() {
-    var ec = buffer.shift();
+    const ec = buffer.shift();
     if (!ec) {
       busy = false;
       return self.drain();
     }
 
-    var sudoc2method; // service used for sudoc request
+    let sudoc2method; // service used for sudoc request
 
     if (issnPattern.test(ec.print_identifier)) {
       self.logger.silly('Sudoc : print_identifier is issn ', ec);
@@ -63,8 +63,8 @@ module.exports = function () {
       return pullBuffer();
     }
 
-    var maxAttempts = 5;
-    var tries = 0;
+    const maxAttempts = 5;
+    let tries = 0;
 
     (function querySudoc() {
       if (++tries > maxAttempts) {
@@ -105,12 +105,12 @@ module.exports = function () {
    * @param  {Object} data     SUDOC data to enrich ECs, if any
    */
   function release(printIdentifier, data) {
-    var result = data && data.query && (data.query.result || data.query.resultNoHolding);
+    const result = data && data.query && (data.query.result || data.query.resultNoHolding);
 
     pending.get(printIdentifier).forEach(function (ec) {
       self.logger.silly('Sudoc : request ', data);
 
-      var ppn = result && result.ppn;
+      const ppn = result && result.ppn;
 
       if (ppn) {
         ec[0]['sudoc-ppn'] = ppn;
