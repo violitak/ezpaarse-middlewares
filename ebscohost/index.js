e6c68ea50075d9ab7b9c0dc2ed3a0d61b978783d
Yannick Schurter <yannick.schurter@gmail.com>
2019-06-19 15:55:08 +0200

    ebscohost db resolver mvp

---
+++ ebscohost/index.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+
+/**
+ * Resolve Ebscohost database titles using the officiel short names list
+ * https://connect.ebsco.com/s/article/EBSCOhost-Database-Short-Names-List?language=en_US
+ */
+module.exports = function () {
+  let list;
+
+  return new Promise((resolve, reject) => {
+    fs.readFile(path.resolve(__dirname, 'list.json'), 'utf8', (err, content) => {
+      if (err) { return reject(err); }
+
+      try {
+        list = JSON.parse(content);
+      } catch (e) {
+        return reject(e);
+      }
+
+      resolve(process);
+    });
+  });
+
+  function process(ec, next) {
+    if (!ec || ec.platform !== 'ebscohost') { return next(); }
+
+    if (ec.db_id && list[ec.db_id.toUpperCase()]) {
+      ec['db_title'] = list[ec.db_id.toUpperCase()];
+    }
+
+    next();
+  }
+};
